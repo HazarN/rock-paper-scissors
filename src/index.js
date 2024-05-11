dd223aa171cf76dc1e98882d0fc23bfff26a82a1
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-05-11 22:29:00 +0300

    fix(core): file directories changed

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
-"use strict";
+'use strict';
 
-const rules       = `<p class="rules">• Rock beats scissor <br>• Paper beats rock<br>• Scissor beats paper</p>`;
-const rulesButton = document.getElementById("rulesButton");
+const rules = `<p class="rules">• Rock beats scissor <br>• Paper beats rock<br>• Scissor beats paper</p>`;
+const rulesButton = document.getElementById('rulesButton');
 
+rulesButton.addEventListener('click', () => {
+  if (document.querySelector('.rules')) {
+    document.querySelector('.rules').remove();
+    document.querySelector('.menu > button:nth-child(3)').remove();
+  }
 
+  const menu = document.querySelector('.menu');
 
-rulesButton.addEventListener("click", () => {
-    if (document.querySelector(".rules")) {
-        document.querySelector(".rules").remove();
-        document.querySelector(".menu > button:nth-child(3)").remove();
-    }
+  // Create a new paragraph element for the rules
+  const rulesParagraph = document.createElement('p');
+  rulesParagraph.innerHTML = rules;
+  rulesParagraph.classList.add('rules');
+  menu.appendChild(rulesParagraph);
 
-    const menu = document.querySelector('.menu');
+  // Create the close button
+  const closeButton = document.createElement('button');
+  closeButton.style.fontSize = '65%';
+  closeButton.innerHTML = 'Close';
+  menu.appendChild(closeButton);
 
-    // Create a new paragraph element for the rules
-    const rulesParagraph     = document.createElement('p');
-    rulesParagraph.innerHTML = rules;
-    rulesParagraph.classList.add('rules');
-    menu.appendChild(rulesParagraph);
-
-    // Create the close button
-    const closeButton          = document.createElement('button');
-    closeButton.style.fontSize = '65%';
-    closeButton.innerHTML      = 'Close';
-    menu.appendChild(closeButton);
-
-    closeButton.addEventListener('click', () => {
-        // Remove the rules paragraph and the close button when the close button is clicked
-        menu.removeChild(rulesParagraph);
-        menu.removeChild(closeButton);
-    });
+  closeButton.addEventListener('click', () => {
+    // Remove the rules paragraph and the close button when the close button is clicked
+    menu.removeChild(rulesParagraph);
+    menu.removeChild(closeButton);
+  });
 });
 
-const gameButton   = document.getElementById("play");
+const gameButton = document.getElementById('play');
 
-gameButton.addEventListener("click", () => {
-    window.location.href = "play.html";
+gameButton.addEventListener('click', () => {
+  window.location.href = 'play.html';
 });
--- src/play.js
+++ src/play.js
@@ -6,95 +6,103 @@ const returnE = document.querySelector('.return');
 const goBackButton = document.querySelector('.go-back');
 const images = document.querySelectorAll('main > *');
 const object = {
-    rock: 1,
-    paper: 2,
-    scissors: 3
+  rock: 1,
+  paper: 2,
+  scissors: 3,
 };
 
 const createImage = (src, alt) => {
-    const img = document.createElement('img');
-    img.src = src;
-    img.alt = alt;
-    return img;
-}
+  const img = document.createElement('img');
+  img.src = src;
+  img.alt = alt;
+  return img;
+};
 const checkWinner = (player, ai) => {
-    returnState = true;
+  returnState = true;
 
-    if (player === ai) {
-        return ['It\'s a tie!', 0];
-    } else if ((player === object.rock && ai === object.scissors) || (player === object.paper && ai === object.rock) || (player === object.scissors && ai === object.paper)) {
-        return ['You Won!', 1];
-    } else {
-        return ['You Lost!', -1];
-    }
-}
+  if (player === ai) {
+    return ["It's a tie!", 0];
+  } else if (
+    (player === object.rock && ai === object.scissors) ||
+    (player === object.paper && ai === object.rock) ||
+    (player === object.scissors && ai === object.paper)
+  ) {
+    return ['You Won!', 1];
+  } else {
+    return ['You Lost!', -1];
+  }
+};
 
 // Game Logic
-images.forEach((image) =>{
-    image.onclick = () => {
-        console.log('clicked');
-        // Preventing multiple clicks
-        if (image.classList.contains('player-picked')) return;
+images.forEach(image => {
+  image.onclick = () => {
+    console.log('clicked');
+    // Preventing multiple clicks
+    if (image.classList.contains('player-picked')) return;
 
-        // Player
-        image.classList.add('player-picked');   
-        images.forEach((image) => {
-            if (!image.classList.contains('player-picked')) {
-                image.classList.add('hidden');
-            }
-        });
-        
-        // AI
-        const number = Math.floor(Math.random() * 3) + 1;
-        let aiPick;
-        if (number === object.rock) {
-            aiPick = createImage('../assets/r.png', 'rock');
-        } else if (number === object.paper) {
-            aiPick = createImage('../assets/p.png', 'paper');
-        } else {
-            aiPick = createImage('../assets/s.png', 'scissors');
-        }
-        document.querySelector('main').appendChild(aiPick);
-        aiPick.classList.add('ai-picked');
+    // Player
+    image.classList.add('player-picked');
+    images.forEach(image => {
+      if (!image.classList.contains('player-picked')) {
+        image.classList.add('hidden');
+      }
+    });
 
-        // Winner
-        const header = document.querySelector('h1'); let status;
-        [header.textContent, status] = checkWinner(object[image.alt], number);
-
-        if (status === 1) {
-            document.querySelector('body').classList.add('win-background');
-        } else if (status === -1) {
-            document.querySelector('body').classList.add('lose-background');
-        } else {
-            document.querySelector('body').classList.add('draw-background');
-        
-        }
+    // AI
+    const number = Math.floor(Math.random() * 3) + 1;
+    let aiPick;
+    if (number === object.rock) {
+      aiPick = createImage('../assets/r.png', 'rock');
+    } else if (number === object.paper) {
+      aiPick = createImage('../assets/p.png', 'paper');
+    } else {
+      aiPick = createImage('../assets/s.png', 'scissors');
     }
+    document.querySelector('main').appendChild(aiPick);
+    aiPick.classList.add('ai-picked');
+
+    // Winner
+    const header = document.querySelector('h1');
+    let status;
+    [header.textContent, status] = checkWinner(object[image.alt], number);
+
+    if (status === 1) {
+      document.querySelector('body').classList.add('win-background');
+    } else if (status === -1) {
+      document.querySelector('body').classList.add('lose-background');
+    } else {
+      document.querySelector('body').classList.add('draw-background');
+    }
+  };
 });
 
 // Go back to the main page
 goBackButton.onclick = () => {
-    window.location.href = "index.html";
-}
+  window.location.href = 'index.html';
+};
 window.onload = () => {
-    if (performance.navigation.type === 1) {
-        const images = document.querySelectorAll('main > *');
-        images.forEach((image) => { image.classList.remove('anim-default');});
-        window.location.href = "index.html";
-    }
-}
+  if (performance.navigation.type === 1) {
+    const images = document.querySelectorAll('main > *');
+    images.forEach(image => {
+      image.classList.remove('anim-default');
+    });
+    window.location.href = 'index.html';
+  }
+};
 
 returnE.onclick = () => {
-    if (returnState) {
-        const aiPick = document.querySelector('.ai-picked');
-        document.querySelector('body').classList.remove('win-background', 'lose-background', 'draw-background');
-        document.querySelector('h1').textContent = 'Rock, Paper, Scissors';
-        images.forEach((image) => {
-            image.classList.remove('player-picked', 'hidden');
-            if (aiPick) {
-                aiPick.remove();
-            }
-        });
-        returnState = false;
-    }
+  if (returnState) {
+    const aiPick = document.querySelector('.ai-picked');
+    document
+      .querySelector('body')
+      .classList.remove('win-background', 'lose-background', 'draw-background');
+    document.querySelector('h1').textContent = 'Rock, Paper, Scissors';
+    images.forEach(image => {
+      image.classList.remove('player-picked', 'hidden');
+      if (aiPick) {
+        aiPick.remove();
+      }
+    });
+    returnState = false;
+  }
 };
